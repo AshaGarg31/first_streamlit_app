788175af1399aff2f885bb3df9f686aaa2037103
AshaGarg31 <129342482+AshaGarg31@users.noreply.github.com>
2023-03-31 15:32:37 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,19 +28,18 @@ streamlit.header("Fruityvice Fruit Advice!")
 #streamlit.text(fruityvice_response.json())
 
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error("Please select a fruit to get information")
-else:
- #streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    #streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-excep URLError as e:
+except URLError as e:
   streamlit.error()
   
 streamlit.stop()
